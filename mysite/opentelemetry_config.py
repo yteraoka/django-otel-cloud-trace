3a4af30e53f8117fc0a787a3186a08fd89877de2
Yoshinori Teraoka <teraoka@3-shake.com>
2023-03-07 00:38:26 +0900

    opentelemetry-resourcedetector-gcp
    機能してるのかどうかわからん

--- mysite/opentelemetry_config.py
+++ mysite/opentelemetry_config.py
@@ -2,7 +2,7 @@ import os
 import json
  
 from opentelemetry import trace
-from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.resources import get_aggregated_resources
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor
@@ -10,26 +10,7 @@ from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.instrumentation.django import DjangoInstrumentor
 from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-
-# Import instrumentation packages here
-
-resource = Resource(attributes={
-    "appsignal.config.language_integration": "python",
-    "appsignal.config.app_path": os.path.dirname(__file__),
-})
-
-provider = TracerProvider(resource=resource)
-trace.set_tracer_provider(provider)
-
-cloud_trace_exporter = CloudTraceSpanExporter()
-if os.getenv('OTEL_SIMPLE_SPAN_PROCESSOR') is not None:
-    trace.get_tracer_provider().add_span_processor(
-        SimpleSpanProcessor(cloud_trace_exporter)
-    )
-else:
-    trace.get_tracer_provider().add_span_processor(
-        BatchSpanProcessor(cloud_trace_exporter)
-    )
+from opentelemetry.resourcedetector.gcp_resource_detector import GoogleCloudResourceDetector
 
 def response_hook(span, request, response):
     span.set_attribute(
@@ -42,6 +23,31 @@ def response_hook(span, request, response):
     pass
 
 def add_instrumentation():
+    #print("DEBUG BEGIN")
+    #print("K_SERVICE".format(os.getenv('K_SERVICE')))
+    #print("K_REVISION".format(os.getenv('K_REVISION')))
+    #for name, value in os.environ.items():
+    #    print("DEBUG: Env {0}: {1}".format(name, value))
+    #print("DEBUG END")
+
+    # MUST be run on a Google tool!
+    # Detect resources from the environment
+    resources = get_aggregated_resources(
+        [GoogleCloudResourceDetector(raise_on_error=False)]
+    )
+
+    trace.set_tracer_provider(TracerProvider(resource=resources))
+
+    cloud_trace_exporter = CloudTraceSpanExporter()
+    if os.getenv('OTEL_SIMPLE_SPAN_PROCESSOR') is not None:
+        trace.get_tracer_provider().add_span_processor(
+            SimpleSpanProcessor(cloud_trace_exporter)
+        )
+    else:
+        trace.get_tracer_provider().add_span_processor(
+            BatchSpanProcessor(cloud_trace_exporter)
+        )
+
     DjangoInstrumentor().instrument(is_sql_commentor_enabled=True, response_hook=response_hook)
     Psycopg2Instrumentor().instrument(enable_commenter=True, commenter_options={})
     RequestsInstrumentor().instrument()
